f75ed0d7be19729b86b1eb0acb276096ab143b23
learnasma <learn.a.m.z77@gmail.com>
2025-11-20 16:20:00 +0300

    add requests

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def home():
     return "Server is running without CLIP!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000 ,debug=False)
